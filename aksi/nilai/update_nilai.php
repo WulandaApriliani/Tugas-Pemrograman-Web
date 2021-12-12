aa55de122c5922920a638d622332630c25481c91
WulandaApriliani <wulaandaapriliani7@gmail.com>
2021-12-12 19:40:57 +0700

    massage

--- aksi/nilai/update_nilai.php
+++ aksi/nilai/update_nilai.php
@@ -7,8 +7,8 @@ $id = $_GET['id'];
 $id_mahasiswa = $_POST['id_mahasiswa'];
 $nilai_akhir = $_POST['nilai_akhir'];
 
-$perintah = "UPDATE `tb_nilai_mahasiswa` SET `id_mahasiswa` = '".$id_mahasiswa. "', `id_mahasiswa` = '".$id_mahasiswa."' WHERE `tb_nilai_mahasiswa`.`id` = " . $id;
-echo $perintah;
+$perintah = "UPDATE `tb_nilai_mahasiswa` SET `id_mahasiswa` = '".$id_mahasiswa. "', `nilai_akhir` = '".$nilai_akhir."' WHERE `tb_nilai_mahasiswa`.`id` = " . $id;
+
 if ($conn->query($perintah) === TRUE) {
     header("location: ../../nilai.php");
 } else {
